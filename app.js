2369c5b91aa8ab6f407d5e922b870a4e6b23ef70
Parzival <parzivalnewton1@gmail.com>
2021-04-03 13:04:54 +0530

    Connected to Database using mongoose

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const app=express();
 
 app.set('view engine','ejs');
-var items=[];
 
 app.use(express.urlencoded({extended:true}));
-
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todoListDB",{useNewUrlParser:true,useUnifiedTopology:true});
+
+const itemSchema=new mongoose.Schema({
+    task:String
+});
+
+const Task=mongoose.model("Task",itemSchema);
+
+const task1=new Task({
+    task:"Buy food"
+})
+const task2=new Task({
+    task:"Eat food"
+})
+const task3=new Task({
+    task:"Store food"
+})
+
+const defaultItems=[task1,task2,task3];
+Task.insertMany(defaultItems,function(err){
+    if(err)console.log(err);
+    else {
+        
+        mongoose.connection.close();
+        console.log("Success");}
+});
+
+
 app.get("/",function(req,res){
-    let dat=new Date();
-    let options={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    }
-    let day=dat.toLocaleDateString("en-US",options);
-    res.render("list.ejs",{currentDay:day,tasks:items});
+    res.render("list.ejs",{currentDay:"Today"});
 })
 
 app.post("/",function(req,res){
-    items.push(req.body.task);
+    //items.push(req.body.task);
     res.redirect("/");
 })
 
